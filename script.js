b212e84ef5ad30049a9f09e54342a516a069e338
Afolayan Tolu Hope <afolayanthopex@gmail.com>
2025-02-08 09:53:45 +0100

    Fixed Vercel deployment issue by adding app.listen()

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_BASE_URL = "https://esp-32-project-backend.vercel.app/api/sensors"; //
 //localhost: https://localhost:5000/api/sensors
 
 
-// const BLYNK_STATUS_URL = "https://blynk.cloud/external/api/get?token=7L6qI3gaecxIK6wMAvNytsvvLya9NyG8&V0"; // Blynk API for system status
+const BLYNK_STATUS_URL = "https://blynk.cloud/external/api/get?token=7L6qI3gaecxIK6wMAvNytsvvLya9NyG8&V0"; // Blynk API for system status
 
 let tempHumidityChart;
 let recentActivity = []; 
